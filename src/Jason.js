899b05f5d954ae3014f9e73eaa8355cbf53261ef
Andrés Cufari <andrescufari@Andress-MacBook-Pro-2.local>
2024-03-13 14:21:54 -0300

    fix JasonBringsComponent

--- src/Jason.js
+++ src/Jason.js
@@ -1,57 +1,50 @@
 import React, { Suspense } from 'react';
 import PropTypes from 'prop-types'; // Adding PropTypes for basic type checking
-
-// Sanitization function to prevent XSS in innerHTML
-const sanitizeInnerHTML = (htmlContent) => {
-  // Simple example: you might want to replace this with a more robust sanitization logic
-  return htmlContent.replace(/<script.*?>.*?<\/script>/gi, '');
-};
+import DOMPurify from 'dompurify';
 
 // The JasonBringsComponent dynamically loads and renders components based on the passed props.
-const JasonBringsComponent = ({ component, type, basePath, attributes, components, innerHTML }) => {
-  if (type === "JComponent") {
-    const DynamicComponent = React.lazy(async () => {
-      const module = await import(`${basePath}${component}`);
-      return { default: module.default };
-    });
+const JasonBringsComponent = ({ component: componentName, attributes, components, componentRegistry, innerHTML }) => {
+  const Component = componentRegistry[componentName] || componentName;
 
-    const nestedComponents = components?.map((c, index) => <JasonBringsComponent key={index} {...c} />);
-
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <DynamicComponent {...attributes}>
-          {nestedComponents}
-        </DynamicComponent>
-      </Suspense>
-    );
-  } else {
-    const Tag = component;
-    const childElements = components?.map((c, index) => <JasonBringsComponent key={index} {...c} />);
-
-    return (
-      <Tag {...attributes}>
-        {innerHTML ? <div dangerouslySetInnerHTML={{ __html: sanitizeInnerHTML(innerHTML) }} /> : childElements}
-      </Tag>
-    );
+  if (!Component) {
+    console.error(`Component ${componentName} not found in registry.`);
+    return null; // Or render a fallback component
   }
+
+  const sanitizedInnerHTML = innerHTML ? DOMPurify.sanitize(innerHTML) : null;
+
+  const nestedComponents = components?.map((c, index) => (
+    <JasonBringsComponent key={index} {...c} componentRegistry={componentRegistry} />
+  ));
+
+  // if innerHTML is provided, wrap nestedComponents in a div that also includes the sanitized innerHTML
+  // Otherwise, just render nestedComponents directly
+  const content = sanitizedInnerHTML
+    ? (<div dangerouslySetInnerHTML={{ __html: sanitizedInnerHTML }} />)
+    : nestedComponents;
+
+  return (
+    <Component {...attributes}>
+      {content}
+    </Component>
+  );
 };
 
 // Adding PropTypes for basic validation
 JasonBringsComponent.propTypes = {
   component: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  basePath: PropTypes.string,
+  type: PropTypes.string,
   attributes: PropTypes.object,
   components: PropTypes.array,
   innerHTML: PropTypes.string,
 };
 
-const JasonCraftThisJSON = ({ json, basePath  = process.env.NEXT_PUBLIC_JASON_BASE_PATH || '/jason/'  }) => {
+const JasonCraftThisJSON = ({ json, componentRegistry = {}}) => {
   return (
     <>
-      {json.components.map((component, index) => (
-        <JasonBringsComponent key={index} basePath={basePath} {...component} />
-      ))}
+      {json.components.map((component, index)  => {
+        return <JasonBringsComponent key={index} componentRegistry={componentRegistry} {...component} />
+      })}
     </>
   );
 };
@@ -61,6 +54,7 @@ JasonCraftThisJSON.propTypes = {
   json: PropTypes.shape({
     components: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
+  componentRegistry: PropTypes.object, // Updated to expect an object
 };
 
 export default JasonCraftThisJSON;
